b71c51f00a7914675760108882a85d2569f18d09
marineesta05 <marineesta05@gmail.com>
2025-12-05 13:06:47 +0100

    Workflow et test OK

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     "no-unused-vars": "warn",
     "no-empty": "warn",
     "no-useless-escape": "warn",
+    "react/react-in-jsx-scope": "off",  // Add this line
   },
   settings: {
     react: { version: "detect" },
